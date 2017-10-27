ac84e494d860cc7b0a8e8643d016c7668b7556a1
Chris <chrisjahim@free.fr>
2017-10-27 11:46:51 +0200

    add: Ajout projet basique SpringBoot

---
+++ src/main/java/fr/rzteam/DirectESport/DirectESportApplication.java
@@ -0,0 +1,12 @@
+package fr.rzteam.DirectESport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirectESportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DirectESportApplication.class, args);
+	}
+}
