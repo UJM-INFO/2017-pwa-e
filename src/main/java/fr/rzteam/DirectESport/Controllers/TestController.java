cf23eefad208bba4e95e0b63b8f5c566cbbc502c
Clement <clement.colin69@gmail.com>
2017-10-30 15:42:01 +0100

    Le controller est mieux nommé

---
+++ src/main/java/fr/rzteam/DirectESport/Controllers/TestController.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.rzteam.DirectESport.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author chris
+ */
+@Controller
+public class TestController
+{
+    
+    @RequestMapping("/url")
+    public String page(Model model)
+    {
+        return "bonjoir";
+    }
+    
+}
