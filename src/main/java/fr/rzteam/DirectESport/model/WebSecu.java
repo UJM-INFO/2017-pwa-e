8db3ffb2cf7a5db8b527d8f5c1946218e43db580
Dimitri Bruyere <dimitri.bruyere@outlook.fr>
2017-10-31 16:17:39 +0100

    fix: web secu

--- src/main/java/fr/rzteam/DirectESport/model/WebSecu.java
+++ src/main/java/fr/rzteam/DirectESport/model/WebSecu.java
@@ -1,8 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * 
  */
+
 package fr.rzteam.DirectESport.model;
 
 import javax.inject.Inject;
@@ -11,32 +10,38 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
+/**
+ *
+ * @author 
+ */
 @Configuration
 @EnableWebSecurity
-public class WebSecu extends WebSecurityConfigurerAdapter 
-{  
-    @Inject
-	UserService userService;
+public class WebSecu extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-            .antMatchers("/home", "/").permitAll()
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+     http.authorizeRequests()
+            .antMatchers("/data", "/").permitAll()
             .antMatchers("/INIT5132827648368438437894").permitAll()
             .anyRequest().authenticated() //.hasAnyRole("ADMIN")
-        .and().formLogin().permitAll()
+     .and().formLogin().permitAll()
              .and().logout().permitAll();
-    }
-    
-    @Inject
-    UserService userDetailsService;
+  }
+  
+  @Inject
+  UserService userDetailsService;
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-            .userDetailsService(userDetailsService)
-            .passwordEncoder(userDetailsService.encoder);
-    }
-
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+      /*
+      auth.inMemoryAuthentication()
+        .withUser("robert").password("toto").roles("USER", "ADMIN")
+        .and().withUser("bob").password("toto").roles("USER");
+*/
+      auth
+              .userDetailsService(userDetailsService)
+              .passwordEncoder(userDetailsService.encoder);
+  }
 }
