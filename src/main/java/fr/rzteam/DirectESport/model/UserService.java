1713a02941646eae6e1802499bbc4105b55ce185
Dimitri Bruyere <dimitri.bruyere@outlook.fr>
2017-11-15 22:10:52 +0100

    feat: function to add an user

--- src/main/java/fr/rzteam/DirectESport/model/UserService.java
+++ src/main/java/fr/rzteam/DirectESport/model/UserService.java
@@ -22,37 +22,58 @@ public class UserService implements UserDetailsService {
     public final PasswordEncoder encoder = new BCryptPasswordEncoder();
     
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException 
+    {
         User u = repo.findOne(username);
         if (u == null) {
             throw new UsernameNotFoundException(username);
         }
         return new org.springframework.security.core.userdetails.User(u.userName, u.password, u.getRoles());
     }
-
-    public void saveUserComputingDerivedPassword(User u, String rawPassword) {
-        setComputingDerivedPassword(u, rawPassword);
-        repo.save(u);
-    }
-
-    public void setComputingDerivedPassword(User u, String rawPassword) {
-        String codedPassword = encoder.encode(rawPassword);
-        u.setPassword(codedPassword);
-    }
-
-    public void makeUserAdmin(String username) {
-        User u = repo.findOne(username);
-        u.getRoles().add(UserRole.ADMIN);
-        repo.save(u);
-    }
-
-    public List<User> listAllUsers() {
-        return repo.findAllByOrderByUserName();
-    }
     
-    public void changeUserPassword(final User user, final String password) 
+    public void addUser(User u)
     {
-        user.setPassword(encoder.encode(password));
-        repo.save(user);
+	if (repo.findByUserName(u.userName) != null)
+	{
+	    System.out.println("This userName is already used.");
+	}
+	else if (repo.findByMail(u.mail) != null)
+	{
+	    System.out.println("There is already an account with this email");
+	}
+	else 
+	{
+	    repo.save(u);
+	}
     }
+//
+//    public void saveUserComputingDerivedPassword(User u, String rawPassword) 
+//    {
+//        setComputingDerivedPassword(u, rawPassword);
+//        repo.save(u);
+//    }
+//
+//    public void setComputingDerivedPassword(User u, String rawPassword) 
+//    {
+//        String codedPassword = encoder.encode(rawPassword);
+//        u.setPassword(codedPassword);
+//    }
+//
+//    public void makeUserAdmin(String username) 
+//    {
+//        User u = repo.findOne(username);
+//        u.getRoles().add(UserRole.ADMIN);
+//        repo.save(u);
+//    }
+//
+//    public List<User> listAllUsers() 
+//    {
+//        return repo.findOneByUserName();
+//    }
+//    
+//    public void changeUserPassword(final User user, final String password) 
+//    {
+//        user.setPassword(encoder.encode(password));
+//        repo.save(user);
+//    }
 }
