fd2e9aa76bbe1280b725446113c0f0abd3f4abe0
Dimitri Bruyere <dimitri.bruyere@outlook.fr>
2017-11-15 22:29:36 +0100

    feat: functions for users

--- src/main/java/fr/rzteam/DirectESport/model/UserService.java
+++ src/main/java/fr/rzteam/DirectESport/model/UserService.java
@@ -43,37 +43,29 @@ public class UserService implements UserDetailsService {
 	}
 	else 
 	{
+	    u.setPassword(encoder.encode(u.password));
 	    repo.save(u);
 	}
     }
-//
-//    public void saveUserComputingDerivedPassword(User u, String rawPassword) 
-//    {
-//        setComputingDerivedPassword(u, rawPassword);
-//        repo.save(u);
-//    }
-//
-//    public void setComputingDerivedPassword(User u, String rawPassword) 
-//    {
-//        String codedPassword = encoder.encode(rawPassword);
-//        u.setPassword(codedPassword);
-//    }
-//
-//    public void makeUserAdmin(String username) 
-//    {
-//        User u = repo.findOne(username);
-//        u.getRoles().add(UserRole.ADMIN);
-//        repo.save(u);
-//    }
-//
-//    public List<User> listAllUsers() 
-//    {
-//        return repo.findOneByUserName();
-//    }
-//    
-//    public void changeUserPassword(final User user, final String password) 
-//    {
-//        user.setPassword(encoder.encode(password));
-//        repo.save(user);
-//    }
+    
+    public void makeUserAdmin(String username) 
+    {
+        User u = repo.findByUserName(username);
+        u.getRoles().add(UserRole.ADMIN);
+        repo.save(u);
+    }
+    
+    public void makeUserEditor(String username) 
+    {
+        User u = repo.findByUserName(username);
+        u.getRoles().add(UserRole.EDITOR);
+        repo.save(u);
+    }
+
+    public void changeUserPassword(String userName, String newPassword) 
+    {
+	User u = repo.findByUserName(userName);
+        u.setPassword(encoder.encode(newPassword));
+        repo.save(u);
+    }
 }
