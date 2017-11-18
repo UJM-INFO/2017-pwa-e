32a0c1d4520d04527e2ba3f25d4ddd022a96fcd2
Chris <chrisjahim@free.fr>
2017-11-18 03:42:15 +0100

    add: websocket test

---
+++ src/main/java/fr/rzteam/DirectESport/websocketTest/Greeting.java
@@ -0,0 +1,18 @@
+package fr.rzteam.DirectESport.websocketTest;
+
+public class Greeting {
+
+    private String content;
+
+    public Greeting() {
+    }
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
