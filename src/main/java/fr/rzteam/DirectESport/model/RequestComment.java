6b4bce1ed224e9690bdd66d0ef358df481730d14
Chris <chrisjahim@free.fr>
2017-11-18 05:31:51 +0100

    add: a model for receive request from Vue.js

---
+++ src/main/java/fr/rzteam/DirectESport/model/RequestComment.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.rzteam.DirectESport.model;
+
+/**
+ *
+ * @author chris
+ */
+public class RequestComment
+{
+    Integer id;
+
+    public Integer getId()
+    {
+        return id;
+    }
+
+    public void setId(Integer id)
+    {
+        this.id = id;
+    }
+    
+    
+}
