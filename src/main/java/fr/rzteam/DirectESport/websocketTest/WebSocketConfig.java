6869c456ec26f8e1e1d2d943645511e80df05d4a
Chris <chrisjahim@free.fr>
2017-11-18 05:33:57 +0100

    refactor: websocket config adaptation from old testing configuration

--- src/main/java/fr/rzteam/DirectESport/websocketTest/WebSocketConfig.java
+++ src/main/java/fr/rzteam/DirectESport/websocketTest/WebSocketConfig.java
@@ -8,17 +8,20 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer
+{
 
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
+    public void configureMessageBroker(MessageBrokerRegistry config)
+    {
         config.enableSimpleBroker("/topic");
         config.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/gs-guide-websocket").withSockJS();
+    public void registerStompEndpoints(StompEndpointRegistry registry)
+    {
+        registry.addEndpoint("/comments-websocket").withSockJS();
     }
 
 }
