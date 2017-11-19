c9ec2bbe95f393310d7a23caebec4a360f4bc939
Chris <chrisjahim@free.fr>
2017-11-19 07:00:04 +0100

    add: transforming methods for Vue, and add/adapted methods for comments

--- src/main/resources/static/js/vue-websocket.js
+++ src/main/resources/static/js/vue-websocket.js
@@ -1,3 +1,5 @@
+//Based on https://spring.io/guides/gs/messaging-stomp-websocket/
+
 var app = new Vue({
     el: '#app',
     data:
@@ -5,48 +7,30 @@ var app = new Vue({
         message: 'Premier test concluant',
         stompClient: null
     },
+    created: function()
+    {
+        //Launch at the opening of the page
+        this.connect();
+    },
     methods:
     {
         connect: function()
         {
+            //Connexion to the websocket server
             var socket = new SockJS('/comments-websocket');
             stompClient = Stomp.over(socket);
             stompClient.connect({}, function (frame)
             {
-                setConnected(true);
-                console.log('Connecté: ' + frame);
+                //Sucess
+                app.showContent();
+            
                 stompClient.subscribe('/topic/comments', function(comments)
                 {
-                    showComments(JSON.parse(comments.body).content);
+                    this.app.showComments(JSON.parse(comments.body));
                 });
-            });
-        },
-        setConnected: function(connected)
-        {
-            $("#connect").prop("disabled", connected);
-            $("#disconnect").prop("disabled", !connected);
-            if(connected)
-            {
-                $("#comments-table").show();
-            }
-            else
-            {
-                $("#comments-table").hide();
-            }
-            $("#comments").html("");
-        },
-        connect: function()
-        {
-            var socket = new SockJS('/comments-websocket');
-            stompClient = Stomp.over(socket);
-            stompClient.connect({}, function (frame)
-            {
-                setConnected(true);
-                console.log('Connecté: ' + frame);
-                stompClient.subscribe('/topic/comments', function (greeting)
-                {
-                    showComments(JSON.parse(greeting.body).content);
-                });
+                
+                //We send the id of the page for begin the data receiving
+                app.sendId();
             });
         },
         disconnect: function()
@@ -55,34 +39,38 @@ var app = new Vue({
             {
                 stompClient.disconnect();
             }
-            setConnected(false);
+            this.app.hideContent();
+            
             console.log("Déconnecté");   
         },
-        sendName: function()
+        showContent()
         {
-            stompClient.send("/app/hello", {}, JSON.stringify({'id': $("#id-comment").val()}));
+            $("#comments-table").show();
+            $("#comments").html("");
         },
-        showComments: function(message)
+        hideContent()
         {
-            $("#comments").append("<tr><td>" + message + "</td></tr>");
+            $("#comments-table").hide();
+            $("#comments").html("");
+        },
+        sendId: function()
+        {
+            //There is also a server verification obvioulsy
+            var id = parseInt($.urlParam('id'));
+            stompClient.send("/app/hello", {}, JSON.stringify({'id': id}));
+        },
+        showComments: function(commentSet)
+        {
+            /*<img src='/dreamhack.jpg' width='300'></img> */
+            
+            var affichage = "<h2><strong>Evenement:</strong> "+commentSet.eventName+"</h2>";
+            commentSet.set.forEach((comment)=>
+            {
+                affichage+=("<p><strong>Commentaire de "+comment.author+"</strong> ("+comment.date+")<br /> "+comment.text+"</p><br/><br/>");
+            });
+            $("#comments").html("");
+            $("#comments").append(affichage);
         }
         
     }
 });
-app.connect();
-
-
-$(function ()
-{
-    $("form").on('submit', function (e)
-    {
-        e.preventDefault();
-    });
-    $( "#connect" ).click(function()
-    { connect(); });
-    $( "#disconnect" ).click(function()
-    { disconnect(); });
-    $( "#send" ).click(function()
-    { sendName(); });
-});
-
