c89299690ce3d83433b7e6d37006c8805e97e640
Dimitri Bruyere <dimitri.bruyere@outlook.fr>
2017-11-22 12:10:38 +0100

    add: class Event.java (without the statistics)

---
+++ src/main/java/fr/rzteam/DirectESport/model/Event.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.rzteam.DirectESport.model;
+
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import lombok.Data;
+
+/**
+ *
+ */
+@Entity
+@Data
+public class Event
+{
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+	
+	Date date; 
+	
+	@ManyToOne
+	Team team1;
+	
+	@ManyToOne
+	Team team2;
+	
+	int status; //0 the game has not started yet / 1 the team1 won / 2 the team2 won / 3 the game is in progress
+}
