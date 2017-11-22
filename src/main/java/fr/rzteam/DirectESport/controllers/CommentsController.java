524e5d94060e89ddf70ff70e483e66d224efec6e
Chris <chrisjahim@free.fr>
2017-11-22 05:32:39 +0100

    refactor: Transforming WS Server in a receptor & sender of signal

--- src/main/java/fr/rzteam/DirectESport/controllers/CommentsController.java
+++ src/main/java/fr/rzteam/DirectESport/controllers/CommentsController.java
@@ -7,6 +7,7 @@ import fr.rzteam.DirectESport.model.RequestComment;
 import javax.inject.Inject;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -17,21 +18,22 @@ public class CommentsController
     CommentRepository repo;
     //WEBSOCKET
     
-    @MessageMapping("/get_comments_by_eventid")
+    @MessageMapping("/update")
     @SendTo("/topic/comments")
-    public CommentSet sendComment(RequestComment request) throws Exception
+    public String sendComment(RequestComment request) throws Exception
     {
+        
             /* on demande au model avec request.getId()*/
 //	repo.save(new Comment("La boule magique !","MisterMV","19/11/2017 06:38"));
 //	repo.save(new Comment("Sa mère la chauve moldave en string latérale, ce gars est trop fort !","Chris","19/11/2017 06:37"));
 //        
 //	repo.save(new Comment("Ces commentaires sont ajoutés directement du serveur dans la classe CommentsController","Chris","19/11/2017 06:49"));
-        CommentSet commentSet= new CommentSet("Evenement "+request.getId(),repo.findAll());
+        //CommentSet commentSet= new CommentSet("Evenement "+request.getId(),repo.findAll());
 //        commentSet.add(new Comment("Ces commentaires sont ajoutés directement du serveur dans la classe CommentsController","Chris","19/11/2017 06:49"));
 //        commentSet.add(new Comment("La boule magique !","MisterMV","19/11/2017 06:38"));
 //        commentSet.add(new Comment("Sa mère la chauve moldave en string latérale, ce gars est trop fort !","Chris","19/11/2017 06:37"));
         
-        return commentSet;
+        return "";
     }
     
     //HTTP
