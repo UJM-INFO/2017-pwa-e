402dcb65d8d2dcad076a8c296236d4bef5be7170
Dimitri Bruyere <dimitri.bruyere@outlook.fr>
2017-11-22 14:24:10 +0100

    add: class Article.java for the articles

---
+++ src/main/java/fr/rzteam/DirectESport/model/Article.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.rzteam.DirectESport.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import lombok.Data;
+
+/**
+ *
+ */
+@Entity
+@Data
+public class Article
+{
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+	
+	String title;
+	
+	String content;
+	
+	@OneToMany(cascade = CascadeType.ALL)
+	List<Comment> comments = new ArrayList<>();
+
+	public Article()
+	{
+	}
+
+	public Article(String title, String content)
+	{
+		this.title = title;
+		this.content = content;
+	}
+	
+	
+}
