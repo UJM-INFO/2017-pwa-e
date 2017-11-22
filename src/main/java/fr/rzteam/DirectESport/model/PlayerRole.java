352b7935efd4077378a494833c1d9aed73b183ec
Dimitri Bruyere <dimitri.bruyere@outlook.fr>
2017-11-22 11:14:58 +0100

    add: class enum PlayerRole.java

---
+++ src/main/java/fr/rzteam/DirectESport/model/PlayerRole.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.rzteam.DirectESport.model;
+
+/**
+ *
+ */
+public enum PlayerRole
+{
+	ADC,
+	Support,
+	Mid,
+	Jungle,
+	Top,
+	Coach;
+}
