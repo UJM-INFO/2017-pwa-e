30637253562d774f706f9e670c9dd1c30c782104
Dimitri Bruyere <dimitri.bruyere@outlook.fr>
2017-11-22 12:23:19 +0100

    add: class Competition.java

---
+++ src/main/java/fr/rzteam/DirectESport/model/Competition.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.rzteam.DirectESport.model;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import lombok.Data;
+
+/**
+ *
+ */
+@Entity
+@Data
+public class Competition
+{
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+	
+	@Temporal(javax.persistence.TemporalType.DATE)
+	Date date;
+	
+	@OneToMany(cascade = CascadeType.ALL)
+	List<Event> events = new ArrayList<>();
+	
+	@ManyToOne
+	Team winner;
+}
