aac71ffb3d116875b7f714d725c08db050a1ff26
Chris <chrisjahim@free.fr>
2017-11-27 22:10:19 +0100

    doc: Javadoc of AdminController

--- src/main/java/fr/rzteam/DirectESport/controllers/AdminController.java
+++ src/main/java/fr/rzteam/DirectESport/controllers/AdminController.java
@@ -1,8 +1,18 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *      _____   _                   _            _____                      _   
+ *     |  __ \ (_) Website Project | |          / ____|                    | |  
+ *     | |  | | _  _ __  ___   ___ | |_    ___ | (___   _ __    ___   _ __ | |_ 
+ *     | |  | || || '__|/ _ \ / __|| __|  / _ \ \___ \ | '_ \  / _ \ | '__|| __|
+ *     | |__| || || |  |  __/| (__ | |_  |  __/ ____) || |_) || (_) || |   | |_ 
+ *     |_____/ |_||_|   \___| \___| \__|  \___||_____/ | .__/  \___/ |_|    \__|
+ *                                                     | |                                                                                              
+ *  ----Authors----                                    |_| 
+ * Dimitri BRUYERE
+ * Clément COLIN
+ * Christopher JEAMME
+ *  ---------------
  */
+
 package fr.rzteam.DirectESport.controllers;
 
 import fr.rzteam.DirectESport.model.User;
@@ -18,7 +28,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
- *
+ * Controller for Admin operations / page
  * @author chris
  */
 @Controller
@@ -27,6 +37,11 @@ public class AdminController
     @Inject
     UserRepository userRepo;
     
+    /**
+     * Display the admin page, the verification is not done here
+     * @param m The model to exchange data with Thymeleaf
+     * @return The template ddmin.html
+     */
     @RequestMapping("/admin")
     public String adminPage(Model m)
     {
@@ -35,6 +50,12 @@ public class AdminController
         return "admin";
     }
     
+    /**
+     * Receiving the order to change a user to the role "User"
+     * @param token A verification for security
+     * @param pseudo Pseudo of the user for changing his role
+     * @return Redirection to "admin"
+     */
     @RequestMapping("/become_user")
     public String becomeUser(
         @RequestParam("token") String token,
@@ -58,6 +79,12 @@ public class AdminController
         return "redirect:/admin";
     }
     
+    /**
+     * Receiving the order to change a user to the role "Editor"
+     * @param token A verification for security
+     * @param pseudo Pseudo of the user for changing his role
+     * @return Redirection to "admin"
+     */
     @RequestMapping("/become_editor")
     public String becomeEditor(
         @RequestParam("token") String token,
@@ -81,6 +108,12 @@ public class AdminController
         return "redirect:/admin";
     }
     
+    /**
+     * Receiving the order to change a user to the role "Admin"
+     * @param token A verification for security
+     * @param pseudo Pseudo of the user for changing his role
+     * @return Redirection to "admin"
+     */
     @RequestMapping("/become_admin")
     public String becomeAdmin(
         @RequestParam("token") String token,
