9c3960bb86d1a882469fc8adf0a850c231f6e50d
Chris <chrisjahim@free.fr>
2017-11-27 18:54:44 +0100

    add: JUnit test for markdown parser

---
+++ src/test/java/fr/rzteam/DirectESport/mdparser/MarkdownTest.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.rzteam.DirectESport.mdparser;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author chris
+ */
+public class MarkdownTest {
+    
+    public MarkdownTest()
+    {
+    }
+
+    /**
+     * Test of parse method, of class Markdown.
+     */
+    @Test
+    public void testParse()
+    {
+        System.out.println("parse");
+        String markdown = "a";
+        String expResult = "<p>a</p>\n";
+        String result = Markdown.parse(markdown);
+        
+        assertEquals(expResult, result);
+    }
+    
+}
