963627db8c2a642fd87e0dec8ff24c29dab1e3be
Dimitri Bruyere <dimitri.bruyere@outlook.fr>
2017-11-29 17:04:16 +0100

    feat: class for verication methods

---
+++ src/main/java/fr/rzteam/DirectESport/verif/FormVerification.java
@@ -0,0 +1,26 @@
+/*
+ *      _____   _                   _            _____                      _   
+ *     |  __ \ (_) Website Project | |          / ____|                    | |  
+ *     | |  | | _  _ __  ___   ___ | |_    ___ | (___   _ __    ___   _ __ | |_ 
+ *     | |  | || || '__|/ _ \ / __|| __|  / _ \ \___ \ | '_ \  / _ \ | '__|| __|
+ *     | |__| || || |  |  __/| (__ | |_  |  __/ ____) || |_) || (_) || |   | |_ 
+ *     |_____/ |_||_|   \___| \___| \__|  \___||_____/ | .__/  \___/ |_|    \__|
+ *                                                     | |                                                                                              
+ *  ----Authors----                                    |_| 
+ * Dimitri BRUYERE
+ * Clément COLIN
+ * Christopher JEAMME
+ *  ---------------
+ */
+package fr.rzteam.DirectESport.verif;
+
+/**
+ * Contains method to verify inputs send by forms
+ */
+public class FormVerification
+{
+    public static boolean verifTextLength(String text, int minLength, int maxLength)
+    {
+        return !(text.length() > maxLength || text.length() < minLength);
+    }
+}
