658f95359e8bb3b83588d78e02494853b887dbe0
Chris <chrisjahim@free.fr>
2017-11-29 05:46:54 +0100

    feat: add method to secure input in the markdown parser

--- src/main/java/fr/rzteam/DirectESport/mdparser/Markdown.java
+++ src/main/java/fr/rzteam/DirectESport/mdparser/Markdown.java
@@ -15,13 +15,10 @@
 package fr.rzteam.DirectESport.mdparser;
 
 import com.vladsch.flexmark.ast.Node;
-import com.vladsch.flexmark.ext.gfm.strikethrough.StrikethroughExtension;
-import com.vladsch.flexmark.ext.tables.TablesExtension;
 import com.vladsch.flexmark.html.HtmlRenderer;
 import com.vladsch.flexmark.parser.Parser;
 import com.vladsch.flexmark.util.options.MutableDataSet;
 
-import java.util.Arrays;
 
 public class Markdown
 {
@@ -33,6 +30,11 @@ public class Markdown
         HtmlRenderer renderer = HtmlRenderer.builder(options).build();
         Node document = parser.parse(markdown);
         String html = renderer.render(document);
-        return html;
+        return secure(html);
+    }
+    
+    public static String secure(String text)
+    {
+        return text.replaceAll("<script", "||CENSORED SCRIPT||");
     }
 }
